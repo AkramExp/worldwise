3a9e9c6c7ac979183f9e62b0f420dd0d3c2baa85
AkramExp <akram.exp02@gmail.com>
2023-10-14 22:06:34 +0530

    add initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+function Homepage() {
+  return (
+    <div>
+      <NavLink to="/pricing">pricing</NavLink>
+    </div>
+  );
+}
+
+export default Homepage;

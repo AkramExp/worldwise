20a7fb45cc68df3d404d6c93621d2ae285ee7020
AkramExp <akram.exp02@gmail.com>
2023-10-15 23:17:31 +0530

    completed pricing page

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,5 +1,27 @@
+import styles from "./Pricing.module.css";
+import PageNav from "../components/PageNav.jsx";
+
 function Pricing() {
-  return <div>Pricing</div>;
+  return (
+    <main className={styles.pricing}>
+      <PageNav />
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="img" className={styles.img} />
+      </section>
+    </main>
+  );
 }
 
 export default Pricing;

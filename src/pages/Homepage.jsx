d7a81d20c74b53ba9aa4628ed32e4dd2b9f1188f
AkramExp <akram.exp02@gmail.com>
2023-10-15 18:39:21 +0530

    completed homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,25 @@
 import { NavLink } from "react-router-dom";
+import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
 function Homepage() {
   return (
-    <div>
-      <NavLink to="/pricing">pricing</NavLink>
-    </div>
+    <main className={styles.homepage}>
+      <PageNav />
+      <section>
+        <h1>
+          You travel the world. <br /> Worldwise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <NavLink to="/app" className="cta">
+          START TRACKING NOW
+        </NavLink>
+      </section>
+    </main>
   );
 }
 

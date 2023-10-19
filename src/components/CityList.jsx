f9ee54fad9677d1043ae01c964c88eea87d627d1
AkramExp <akram.exp02@gmail.com>
2023-10-19 21:45:06 +0530

    added spinner while data is fetching

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,25 +1,32 @@
 import { useEffect, useState } from "react"
 import styles from "./CityList.module.css"
 import CityItem from "./CityItem";
+import Spinner from "./Spinner";
 
 export default function CityList() {
     const BASE_URL = "http://localhost:8001";
     const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(function () {
         async function fetchCities() {
             try {
+                setIsLoading(true);
                 const res = await fetch(`${BASE_URL}/cities`);
                 const data = await res.json();
                 console.log(data);
                 setCities(data);
             } catch (err) {
                 console.log(err.message);
+            } finally {
+                setIsLoading(false);
             }
         }
 
         fetchCities();
     }, [])
+
+    if (isLoading) return <Spinner />
 
     return (
         <ul className={styles.cityList}>
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react"
 import styles from "./CountryList.module.css"
 import CountryItem from "./CountryItem";
+import Spinner from "./Spinner";
 
 export default function CountryList() {
     const BASE_URL = "http://localhost:8001";
     const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(function () {
         async function fetchCities() {
             try {
+                setIsLoading(true);
                 const res = await fetch(`${BASE_URL}/cities`);
                 const data = await res.json();
                 console.log(data);
                 setCities(data);
             } catch (err) {
                 console.log(err.message);
+            } finally {
+                setIsLoading(false);
             }
         }
 
@@ -26,6 +31,8 @@ export default function CountryList() {
             return [...arr, { country: city.country, emoji: city.emoji }];
         else return arr;
     }, [])
+
+    if (isLoading) return <Spinner />
 
     return (
         <ul className={styles.countryList}>{countries.map(country => <CountryItem country={country} />)}</ul>

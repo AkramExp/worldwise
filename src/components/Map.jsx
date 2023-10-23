330444f984e6a6e22a7c2b1d33b183abeb5b96be
AkramExp <akram.exp02@gmail.com>
2023-10-23 15:39:14 +0530

    added geo location custom hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,39 +1,69 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 import {
     MapContainer,
     TileLayer,
     Marker,
     Popup,
     useMapEvent,
+    useMap,
 } from "react-leaflet";
+import { useEffect, useState } from "react";
 
 export default function Map() {
 
     const { cities } = useCities();
+    const [mapPosition, setMapPosition] = useState([0, 40]);
+    const [searchParams] = useSearchParams();
 
-    const position = [0, 40];
+    const mapLat = searchParams.get('lat');
+    const mapLng = searchParams.get('lng');
+
+    const { isLoading: isLoadingPosition,
+        position: geolocationPosition,
+        getPosition } = useGeolocation();
+
+    useEffect(function () {
+        if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    }, [mapLat, mapLng])
+
+    useEffect(function () {
+        if (geolocationPosition)
+            setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+    }, [geolocationPosition]);
 
     return (
         <div className={styles.mapContainer}>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={true} className={styles.map} >
+            {!geolocationPosition && <Button type='position' onClick={getPosition}>
+                {isLoadingPosition ? 'Loading..' : 'Get your position'}
+            </Button>}
+            <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map} >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {
                     cities.map(city =>
-                        <Marker position={city.position}>
+                        <Marker position={city.position} key={city.id}>
                             <Popup>
                                 {city.cityName}
                             </Popup>
                         </Marker>)
                 }
+                <ChangeCenter position={mapPosition} />
                 <DetectClick />
             </MapContainer>
         </div>
     )
+}
+
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
 }
 
 function DetectClick() {

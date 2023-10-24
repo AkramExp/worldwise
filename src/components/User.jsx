929bb6e1c4356edbef5fbb7126cccd99e0c1e40c
AkramExp <akram.exp02@gmail.com>
2023-10-24 18:26:00 +0530

    added logout functionality

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,22 @@
+import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./User.module.css";
+import { useNavigate } from "react-router-dom";
 
-export default function User(user) {
+export default function User() {
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+
+    function handleClick(e) {
+        e.preventDefault();
+
+        logout();
+        navigate('/')
+    }
     return (
         <div className={styles.user}>
             <img src={user.avatar} alt={user.name} />
             <span>Welcome, {user.name}</span>
-            <button>Logout</button>
+            <button onClick={handleClick}>Logout</button>
         </div>
     )
 }
